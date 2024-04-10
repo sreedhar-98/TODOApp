ff9087eb3cb695c933f4a3a3435b9b5af95e2489
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-04-10 11:17:34 +0530

    Sort

--- Frontend/todo-app/src/Components/TODOList.js
+++ Frontend/todo-app/src/Components/TODOList.js
@@ -12,6 +12,7 @@ const TODOList = () => {
   const { data, isSuccess, isLoading,isError } = useGetTodosQuery();
 
   if (isLoading || isError) return;
+  
 
   const todo_filtered_data = data["todos"].filter((todo) => {
     return !todo.completed;
@@ -27,16 +28,15 @@ const TODOList = () => {
         {isTodo && <AddButton />}
         <div>
           <select
-            defaultValue={""}
+            defaultValue={"datehigh"}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
              focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
               dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
-            <option value={""} disabled hidden>
-              Sort
-            </option>
-            <option value={"CreatedDate"}>Created Date</option>
-            <option value={"Priority"}>Priority</option>
+            <option value={"datehigh"}>Created Date (Latest to Oldest)</option>
+            <option value={"datelow"}>Created Date (Oldest to Latest)</option>
+            <option value={"priorityhigh"}>Priority (High to Low)</option>
+            <option value={"prioritylow"}>Priority (Low to High)</option>
           </select>
         </div>
       </div>
